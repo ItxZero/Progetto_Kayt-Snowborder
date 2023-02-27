13248e0ac5878ad9a8c32172fd5e6a553f592d66
ItxZero <riper.grimmer@gmail.com>
2023-02-27 09:38:25 +0100

    adjust 4

--- Kayt_snowboarding/main.cpp
+++ Kayt_snowboarding/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include <time.h>
+#include <fstream>
 
 using namespace std;
 
@@ -9,18 +10,34 @@ struct caratteristicheSnowboarder{
     int ya=0;
     int xb;
     int yb;
+    int distanzaTot;
     string cognome;
     string matricola;
 };
 
 void generazioneCoordinateECalcolo(caratteristicheSnowboarder snowboarder){
-    srand(time(NULL));
+    ifstream fin("ListaCognomi.txt", ios::in);
+    ofstream fout("ListaCognomi.txt", ios::out);
     float distanzaParziale;
+    caratteristicheSnowboarder appo[10];
 
-    for(int i = 0; i < 30; i++){
-        snowboarder.xb = rand()%101;
-        snowboarder.yb = rand()%101;
-        distanzaParziale = sqrt(pow((snowboarder.xb-snowboarder.xa),2)+pow((snowboarder.yb-snowboarder.ya),2));
+    while(!fin.eof()){
+        int i=0,matric;
+        getline(fin,appo[i].cognome); //lettura dei cognomi dal file
+        matric = rand()%200;
+        appo[i].matricola = matric; //immissione di una matricola casuale nel vettore
+        i++;
+    }
+
+    srand(time(NULL));
+
+    for(int j = 0; j < 10; j++){
+        for(int i = 0; i < 30; i++){
+            snowboarder.xb = rand()%101;
+            snowboarder.yb = rand()%101;
+            distanzaParziale = sqrt(pow((snowboarder.xb-snowboarder.xa),2)+pow((snowboarder.yb-snowboarder.ya),2)); //calcolo della distanza
+        }
+        appo[j].distanzaTot = distanzaParziale;
     }
 
 }
