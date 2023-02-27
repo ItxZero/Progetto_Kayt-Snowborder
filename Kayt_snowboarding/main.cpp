49e86acd41fce314ab86f41cd08a61496f27023d
ItxZero <riper.grimmer@gmail.com>
2023-02-27 10:04:47 +0100

    adjust 5

--- Kayt_snowboarding/main.cpp
+++ Kayt_snowboarding/main.cpp
@@ -6,49 +6,54 @@
 using namespace std;
 
 struct caratteristicheSnowboarder{
-    int xa=0;
-    int ya=0;
-    int xb;
-    int yb;
+    int x[30];
+    int y[30];
     int distanzaTot;
     string cognome;
     string matricola;
 };
 
-void generazioneCoordinateECalcolo(caratteristicheSnowboarder snowboarder){
+void generazioneCoordinateECalcolo(caratteristicheSnowboarder snowboarder[10]){
     ifstream fin("ListaCognomi.txt", ios::in);
     ofstream fout("ListaCognomi.txt", ios::out);
+    int distanzaAttuale = 0;
     float distanzaParziale;
-    caratteristicheSnowboarder appo[10];
+
 
     while(!fin.eof()){
         int i=0,matric;
-        getline(fin,appo[i].cognome); //lettura dei cognomi dal file
+        getline(fin,snowboarder[i].cognome); //lettura dei cognomi dal file
         matric = rand()%200;
-        appo[i].matricola = matric; //immissione di una matricola casuale nel vettore
+        snowboarder[i].matricola = matric; //immissione di una matricola casuale nel vettore
         i++;
     }
 
     srand(time(NULL));
 
     for(int j = 0; j < 10; j++){
-        for(int i = 0; i < 30; i++){
-            snowboarder.xb = rand()%101;
-            snowboarder.yb = rand()%101;
-            distanzaParziale = sqrt(pow((snowboarder.xb-snowboarder.xa),2)+pow((snowboarder.yb-snowboarder.ya),2)); //calcolo della distanza
+        snowboarder[j].x[0] = 0;
+        snowboarder[j].y[0] = 0;
+        for(int i = 1; i < 30; i++){
+            snowboarder[j].x[i] = rand()%101;
+            snowboarder[j].y[i] = rand()%101;
+            distanzaParziale = sqrt(pow((snowboarder[j].x[i]-snowboarder[j].x[i-1]),2)+pow((snowboarder[j].y[i]-snowboarder[j].y[i-1]),2)); //calcolo della distanza parziale
+            distanzaAttuale = distanzaAttuale + distanzaParziale;
         }
-        appo[j].distanzaTot = distanzaParziale;
+        snowboarder[j].distanzaTot = distanzaAttuale;
     }
 
 }
 
 void stampaLista(){
+    cout<<"     Podio   ";
+
+    cout<<"     Podio   ";
 
 }
 
 int main()
 {
-    caratteristicheSnowboarder snowboarder;
+    caratteristicheSnowboarder snowboarder[10];
     int scelta;
 
     while(true){
